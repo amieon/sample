6683510df0a0d754b9f9c7f499a6dc6e2a34a834
for9int <wtmsb3770@gmail.com>
2024-07-17 14:40:15 +0800

    加入关于mod的逆元，与exgcd函数

--- tmp.cpp
+++ tmp.cpp
@@ -3,6 +3,16 @@ using namespace std;
 const int maxn = 107;
 const int mod = 114514;
 int prime[maxn],tot,phi[maxn],sumphi[maxn];
+int inv[maxn];
+int exgcd(int a,int b,int &x,int &y){
+    if(b == 0){
+        x = 1, y = 0;
+        return a;
+    }
+    int d = exgcd(b,a%b,y,x);
+    y -= a/b*x;
+    return d;
+}
 int main(){
     cout << "oh year~\n";
     cout << "oh year~\n";
@@ -20,6 +30,9 @@ int main(){
                 phi[i * prime[j]] = phi[i] * (prime[j]-1);
         }
     }
+    inv[1] = 1;
+    for(int i=2;i<maxn;++i)
+        inv[i] = mod - inv[i%mod] * (mod / i) % mod;
     for(int i = 1;i<=tot;++i)
         cout << prime[i] << ' ';
     cout << endl;
@@ -28,6 +41,9 @@ int main(){
     cout << endl;
     for(int i = 1;i<maxn;++i)
         cout << sumphi[i] << ' ';
+    cout << endl;
+    for(int i = 1;i<maxn;++i)
+        cout << inv[i] << ' ';
     cout << endl;
     return 0;
 }
